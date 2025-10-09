64ab59df15016d06d6a7272d583dd26628eb54ba
TiaaaFitria <24111814082@mhs.unesa.ac.id>
2025-10-09 14:49:02 +0700

    Add files via upload

--- lib/model/app_data.dart
+++ lib/model/app_data.dart
@@ -1,90 +1,53 @@
 import 'package:flutter/material.dart';
-import 'menu_item.dart';  
+import '../model/menu_item.dart';
+
+// import halaman
 import '../pages/menu/mood_tracker_page.dart';
 import '../pages/menu/journal_page.dart';
 import '../pages/menu/reminder_page.dart';
-import '../pages/menu/daily_quotes_page.dart';
 import '../pages/menu/dear_me_page.dart';
 import '../pages/menu/journey_page.dart';
+import '../pages/menu/daily_quotes_page.dart';
 
-class AppData extends ChangeNotifier {
-  final List<Map<String, dynamic>> _moodHistory = [];
-  List<Map<String, dynamic>> get moodHistory => _moodHistory;
-
-  void addMood(Map<String, dynamic> mood) {
-    _moodHistory.insert(0, mood);
-    notifyListeners();
-  }
-
-  final List<Map<String, dynamic>> _journalEntries = [];
-  List<Map<String, dynamic>> get journalEntries => _journalEntries;
-
-  void addJournal(Map<String, dynamic> journal) {
-    _journalEntries.insert(0, journal);
-    notifyListeners();
-  }
-
-  final List<String> _reminders = [];
-  List<String> get reminders => _reminders;
-
-  void addReminder(String reminder) {
-    _reminders.add(reminder);
-    notifyListeners();
-  }
-
-  void removeReminder(int index) {
-    _reminders.removeAt(index);
-    notifyListeners();
-  }
-
-  int get journalEntriesCount => _journalEntries.length;
-  int get remindersCompleted => _reminders.length;
-
-  final List<MenuItem> _menuItems = [
-    MenuItem(
-      title: "Mood Tracker",
-      icon: Icons.mood,
-      color: Colors.teal,
-      page: const MoodTrackerPage(userId: 'dummyUser123'),
-    ),
-    MenuItem(
-      title: "My Journal",
-      icon: Icons.book,
-      color: Colors.blue,
-      page: const JournalPage(userId: 'dummyUser123'),
-    ),
-    MenuItem(
-      title: "Reminder",
-      icon: Icons.alarm,
-      color: Colors.orange,
-      page: const ReminderPage(userId: 'dummyUser123'),
-    ),
-    MenuItem(
-      title: "Daily Quotes",
-      icon: Icons.format_quote,
-      color: Colors.purple,
-      page: const DailyQuotesPage(userId: 'dummyUser123'),
-    ),
-    MenuItem(
-      title: "Dear Me",
-      icon: Icons.mail,
-      color: Colors.pink,
-      page: const DearMePage(userId: 'dummyUser123'),
-    ),
-    MenuItem(
-      title: "My Journey",
-      icon: Icons.timeline,
-      color: Colors.green,
-      page: const JourneyPage(userId: 'dummyUser123'),
-    ),
-  ];
-
-  List<MenuItem> get menuItems => _menuItems;
-
-  set menuItems(List<MenuItem> items) {
-    _menuItems
-      ..clear()
-      ..addAll(items);
-    notifyListeners();
+class AppData {
+  List<MenuItem> getMenuItems(String userId) {
+    return [
+      MenuItem(
+        title: "Mood Tracker",
+        icon: Icons.mood,
+        color: Colors.blueAccent,
+        pageBuilder: () => const MoodTrackerPage(userId: '',),
+      ),
+      MenuItem(
+        title: "My Journal",
+        icon: Icons.book_rounded,
+        color: Colors.greenAccent,
+        pageBuilder: () => const JournalPage(userId: '',),
+      ),
+      MenuItem(
+        title: "Reminder",
+        icon: Icons.alarm,
+        color: Colors.teal,
+        pageBuilder: () => const ReminderPage(userId: '',),
+      ),
+      MenuItem(
+        title: "Daily Quotes",
+        icon: Icons.format_quote_rounded,
+        color: Colors.purpleAccent,
+        pageBuilder: () => DailyQuotesPage(userId: userId),
+      ),
+      MenuItem(
+        title: "Dear Me",
+        icon: Icons.favorite_rounded,
+        color: Colors.pinkAccent,
+        pageBuilder: () => const DearMePage(userId: '',),
+      ),
+      MenuItem(
+        title: "My Journey",
+        icon: Icons.timeline_rounded,
+        color: Colors.lightBlueAccent,
+        pageBuilder: () => JourneyPage(userId: userId),
+      ),
+    ];
   }
 }
--- lib/model/user_preferences.dart
+++ lib/model/user_preferences.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_buddy/model/menu_item.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'user_model.dart';
 
@@ -8,6 +9,7 @@ class UserPreferences {
   static const String _keyPassword = "user_password";
   static const String _keyPhotoPath = "user_photoPath";
 
+  // Simpan semua data user
   static Future<void> saveUser(UserModel user) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setString(_keyId, user.id);
@@ -21,15 +23,15 @@ class UserPreferences {
     }
   }
 
+  // Ambil semua data user
   static Future<UserModel?> getUser() async {
     final prefs = await SharedPreferences.getInstance();
-
     final id = prefs.getString(_keyId);
     final name = prefs.getString(_keyName);
     final email = prefs.getString(_keyEmail);
 
     if (id == null || name == null || email == null) {
-      return null; // belum ada data user
+      return null;
     }
 
     return UserModel(
@@ -41,62 +43,31 @@ class UserPreferences {
     );
   }
 
+  // Hapus semua data user
   static Future<void> clearUser() async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.remove(_keyId);
-    await prefs.remove(_keyName);
-    await prefs.remove(_keyEmail);
-    await prefs.remove(_keyPassword);
-    await prefs.remove(_keyPhotoPath);
+    await prefs.clear();
   }
 
-  static Future<void> setId(String id) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setString(_keyId, id);
-  }
-
-  static Future<void> setName(String name) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setString(_keyName, name);
-  }
-
-  static Future<void> setEmail(String email) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setString(_keyEmail, email);
-  }
-
-  static Future<void> setPassword(String password) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setString(_keyPassword, password);
-  }
-
+  // Setter individual
   static Future<void> setPhotoPath(String photoPath) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setString(_keyPhotoPath, photoPath);
   }
 
-  static Future<String?> getId() async {
-    final prefs = await SharedPreferences.getInstance();
-    return prefs.getString(_keyId);
-  }
-
-  static Future<String?> getName() async {
-    final prefs = await SharedPreferences.getInstance();
-    return prefs.getString(_keyName);
-  }
-
-  static Future<String?> getEmail() async {
-    final prefs = await SharedPreferences.getInstance();
-    return prefs.getString(_keyEmail);
-  }
-
-  static Future<String?> getPassword() async {
-    final prefs = await SharedPreferences.getInstance();
-    return prefs.getString(_keyPassword);
-  }
-
+  // Getter individual
   static Future<String?> getPhotoPath() async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getString(_keyPhotoPath);
   }
+
+  static Future getQuote() async {}
+
+  static Future getMenus() async {}
+
+  static Future<void> saveQuote(String editableQuote) async {}
+
+  static Future<void> saveMenus(List<MenuItem> menuItems) async {}
+
+  static Future<void> saveUserRole(String s) async {}
 }
